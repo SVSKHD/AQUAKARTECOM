81b0a38e26c562a8b35de7b1d1e7eec409086090
SVSKHD <8svskhd@gmail.com>
2024-06-15 00:10:05 +0530

    components

---
+++ src/app/components/reusables/seo.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+
+interface SEOProps {
+  title?: string;
+  description?: string;
+  keywords?: string;
+  author?: string;
+  ogTitle?: string;
+  ogDescription?: string;
+  ogImage?: string;
+  ogUrl?: string;
+  twitterTitle?: string;
+  twitterDescription?: string;
+  twitterImage?: string;
+}
+
+const AquaSeo: React.FC<SEOProps> = ({
+  title = 'Default Title',
+  description = 'Default description',
+  keywords = 'default, keywords',
+  author = 'Default Author',
+  ogTitle,
+  ogDescription,
+  ogImage,
+  ogUrl,
+  twitterTitle,
+  twitterDescription,
+  twitterImage,
+}) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content={author} />
+
+      {/* Open Graph */}
+      {ogTitle && <meta property="og:title" content={ogTitle} />}
+      {ogDescription && <meta property="og:description" content={ogDescription} />}
+      {ogImage && <meta property="og:image" content={ogImage} />}
+      {ogUrl && <meta property="og:url" content={ogUrl} />}
+      <meta property="og:type" content="website" />
+
+      {/* Twitter */}
+      {twitterTitle && <meta name="twitter:title" content={twitterTitle} />}
+      {twitterDescription && <meta name="twitter:description" content={twitterDescription} />}
+      {twitterImage && <meta name="twitter:image" content={twitterImage} />}
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
+  );
+};
+
+export default AquaSeo;
