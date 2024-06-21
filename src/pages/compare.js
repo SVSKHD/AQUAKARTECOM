5078ea1cb80acea7bbe5efb7688369715b79b2b2
SVSKHD <8svskhd@gmail.com>
2024-06-21 07:45:21 +0530

    compare

--- src/pages/compare.js
+++ src/pages/compare.js
@@ -1,9 +1,17 @@
+import { useState } from "react"
+import AquaReuseDrawer from "@/components/reusables/drawer"
 import AquaCompareIndexComponent from "@/pageComponents/compare"
 
+
 const AquaCompare = () =>{
+    const [check , setCheck] = useState(false)
+    const seo={
+        title:"hello"
+    }
 return(
-    <AquaCompareIndexComponent>
-        
+    <AquaCompareIndexComponent seo={seo}>
+        <AquaReuseDrawer open={check}/>
+        <button onClick={()=>setCheck(!check)}>open</button>
     </AquaCompareIndexComponent>
 )
 }
