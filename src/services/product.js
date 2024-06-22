1fe565b5d441158dcbc5e95740f2879244a7a7e5
SVSKHD <8svskhd@gmail.com>
2024-06-22 08:28:52 +0530

    prodcut services

---
+++ src/services/product.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const BASE = process.env.NEXT_PUBLIC_API_URL 
+
+const AllProducts = () =>axios.get(`${BASE}/all-products`)
+const ProductById = (id)=>axios.get(`${BASE}/product/${id}`)
+
+
+const ProductServiceOperations={
+AllProducts,
+ProductById
+}
+export default ProductServiceOperations
