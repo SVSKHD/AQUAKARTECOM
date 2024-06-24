50e2ac57154efc96d1f7f8ed8255a5775eea1922
SVSKHD <8svskhd@gmail.com>
2024-06-24 23:26:00 +0530

    ecom integrations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 import { useStore } from "react-redux";
+import { useRouter } from "next/router";
 
 const persistConfig = {
   key: "root",
@@ -19,6 +20,51 @@ const store = createStore(persistedReducer);
 const persistor = persistStore(store);
 
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Function to initialize gtag
+    function gtag() {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push(arguments);
+    }
+
+    // Check if script is already loaded
+    if (!window.gtag) {
+      // Create script element
+      const script = document.createElement("script");
+      script.src = `https://www.googletagmanager.com/gtag/js?id=G-FS41RRVRD4`;
+      script.async = true;
+      document.head.appendChild(script);
+
+      // Initialize dataLayer and configure gtag
+      script.onload = () => {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        window.gtag = gtag;
+        gtag("js", new Date());
+        gtag("config", "G-FS41RRVRD4");
+      };
+    }
+
+    // Route change handler
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-FS41RRVRD4", {
+        page_path: url,
+      });
+    };
+
+    // Subscribe to route changes
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // Cleanup on unmount
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   // This useEffect will run only on the client side
   useEffect(() => {
     if (typeof window !== "undefined") {
