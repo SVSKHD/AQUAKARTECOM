1801dcf23994016043e56ffd0d30a0c3fd3985b8
SVSKHD <8svskhd@gmail.com>
2024-07-06 19:03:02 +0530

    mobile-otp

--- src/components/common/commonDialogs/commonAuth/mobileOtp.js
+++ src/components/common/commonDialogs/commonAuth/mobileOtp.js
@@ -44,6 +44,9 @@ const AquaAuthMobileForm = ({ signup }) => {
                 className="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
+            <p className="mt-2 text-sm text-gray-500">
+              Please ensure your phone number is associated with WhatsApp.
+            </p>
           </div>
 
           <div>
