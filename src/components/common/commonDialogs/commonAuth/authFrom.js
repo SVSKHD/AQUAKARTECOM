328bcd0711b72a0c67331b1fb312dfcacf12d392
SVSKHD <8svskhd@gmail.com>
2024-07-21 23:30:32 +0530

    auth form

--- src/components/common/commonDialogs/commonAuth/authFrom.js
+++ src/components/common/commonDialogs/commonAuth/authFrom.js
@@ -111,7 +111,7 @@ const AquaAuthMobileForm = ({ signup }) => {
                 value={email}
                 onChange={handleEmailChange}
                 placeholder="example@example.com"
-                className="block w-full p-4 rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full p-4 rounded-md border-0 py-1.5 pr-10 text-gray-900 bg-white text-gray-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
             {otpShow && (
--- src/components/common/commonDialogs/commonAuth/mobileOtp.js
+++ src/components/common/commonDialogs/commonAuth/mobileOtp.js
@@ -117,7 +117,7 @@ const AquaAuthMobileForm = ({ signup }) => {
                 value={phone}
                 onChange={handlePhoneChange}
                 placeholder="000-00-00000"
-                className="block w-full p-4 rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full p-4 rounded-md border-0 py-1.5 pr-10 text-gray-900 bg-white text-gray-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
             {otpShow && (
@@ -130,7 +130,7 @@ const AquaAuthMobileForm = ({ signup }) => {
                   value={otp}
                   onChange={(e) => setOtp(e.target.value)}
                   placeholder="Enter OTP"
-                  className="block w-full p-4 rounded-md border-0 py-1.5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full p-4 rounded-md border-0 py-1.5 pr-10 text-gray-900 bg-white text-gray-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             )}
