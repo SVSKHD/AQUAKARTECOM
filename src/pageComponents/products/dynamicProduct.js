4f36cbcacd6316ad3d136303c18e30c333faa77a
SVSKHD <8svskhd@gmail.com>
2024-08-17 22:35:01 +0530

    dynamic product

--- src/pageComponents/products/dynamicProduct.js
+++ src/pageComponents/products/dynamicProduct.js
@@ -102,63 +102,63 @@ export default function AquaDynamicProductComponent() {
       <div className="bg-white">
         <main className="mx-auto max-w-7xl sm:px-6 sm:pt-16 lg:px-8">
           <div className="mx-auto max-w-2xl lg:max-w-none">
-            {/* Product */}
-            <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
               {/* Image gallery */}
-              <TabGroup className="flex flex-col-reverse">
-                {/* Image selector */}
-                <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-                  <TabList className="grid grid-cols-4 gap-6">
-                    {productData?.photos?.map((image) => (
-                      <Tab
-                        key={image.id}
-                        className="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
-                      >
-                        {({ selected }) => (
-                          <>
-                            <span className="sr-only">{image.id}</span>
-                            <span className="absolute inset-0 overflow-hidden rounded-md">
-                              <Image
-                                src={image.secure_url}
-                                alt=""
-                                height={600}
-                                width={400}
-                                className="h-full w-full object-cover object-center"
+              <div className="sticky top-0">
+                <TabGroup className="flex flex-col-reverse">
+                  <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+                    <TabList className="grid grid-cols-4 gap-6">
+                      {productData?.photos?.map((image) => (
+                        <Tab
+                          key={image.id}
+                          className="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span className="sr-only">{image.id}</span>
+                              <span className="absolute inset-0 overflow-hidden rounded-md">
+                                <Image
+                                  src={image.secure_url}
+                                  alt=""
+                                  height={600}
+                                  width={400}
+                                  className="h-full w-full object-cover object-center"
+                                />
+                              </span>
+                              <span
+                                className={classNames(
+                                  selected
+                                    ? "ring-indigo-500"
+                                    : "ring-transparent",
+                                  "pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2",
+                                )}
+                                aria-hidden="true"
                               />
-                            </span>
-                            <span
-                              className={classNames(
-                                selected
-                                  ? "ring-indigo-500"
-                                  : "ring-transparent",
-                                "pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2",
-                              )}
-                              aria-hidden="true"
-                            />
-                          </>
-                        )}
-                      </Tab>
-                    ))}
-                  </TabList>
-                </div>
+                            </>
+                          )}
+                        </Tab>
+                      ))}
+                    </TabList>
+                  </div>
 
-                <TabPanels className="aspect-h-1 aspect-w-1 w-full">
-                  {productData.photos.map((image) => (
-                    <TabPanel key={image.id}>
-                      <Image
-                        src={image.secure_url}
-                        alt={image.alt}
-                        className="h-full w-full object-cover object-center sm:rounded-lg"
-                        height={450}
-                        width={300}
-                      />
-                    </TabPanel>
-                  ))}
-                </TabPanels>
-              </TabGroup>
+                  <TabPanels className="aspect-h-1 aspect-w-1 w-full">
+                    {productData.photos.map((image) => (
+                      <TabPanel key={image.id}>
+                        <Image
+                          src={image.secure_url}
+                          alt={image.alt}
+                          className="h-full w-full object-cover object-center sm:rounded-lg"
+                          height={450}
+                          width={300}
+                        />
+                      </TabPanel>
+                    ))}
+                  </TabPanels>
+                </TabGroup>
+              </div>
 
               {/* Product info */}
-              <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0 lg:ml-8 lg:max-h-[calc(100vh-20px)] lg:overflow-y-auto no-scrollbar">
                 <h1 className="text-3xl font-bold tracking-tight text-gray-900">
                   {productData.title}
                 </h1>
@@ -170,30 +170,7 @@ export default function AquaDynamicProductComponent() {
                   </p>
                 </div>
 
-                {/* Reviews */}
-                {/* <div className="mt-3">
-                  <h3 className="sr-only">Reviews</h3>
-                  <div className="flex items-center">
-                    <div className="flex items-center">
-                      {[0, 1, 2, 3, 4].map((rating) => (
-                        <StarIcon
-                          key={rating}
-                          className={classNames(
-                            product.rating > rating
-                              ? "text-indigo-500"
-                              : "text-gray-300",
-                            "h-5 w-5 flex-shrink-0",
-                          )}
-                          aria-hidden="true"
-                        />
-                      ))}
-                    </div>
-                    <p className="sr-only">{product.rating} out of 5 stars</p>
-                  </div>
-                </div> */}
-
                 <div className="mt-6">
-                  <h3 className="sr-only">Description</h3>
                   <div className="mt-10 mb-5 flex">
                     <button
                       type="submit"
@@ -239,100 +216,10 @@ export default function AquaDynamicProductComponent() {
                     }}
                   />
                 </div>
-
-                <form className="mt-6">
-                  {/* Colors */}
-                  {/* <div>
-                    <h3 className="text-sm text-gray-600">Color</h3>
-
-                    <fieldset aria-label="Choose a color" className="mt-2">
-                      <RadioGroup
-                        value={selectedColor}
-                        onChange={setSelectedColor}
-                        className="flex items-center space-x-3"
-                      >
-                        {product.colors.map((color) => (
-                          <Radio
-                            key={color.name}
-                            value={color}
-                            aria-label={color.name}
-                            className={({ focus, checked }) =>
-                              classNames(
-                                color.selectedColor,
-                                focus && checked ? "ring ring-offset-1" : "",
-                                !focus && checked ? "ring-2" : "",
-                                "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none",
-                              )
-                            }
-                          >
-                            <span
-                              aria-hidden="true"
-                              className={classNames(
-                                color.bgColor,
-                                "h-8 w-8 rounded-full border border-black border-opacity-10",
-                              )}
-                            />
-                          </Radio>
-                        ))}
-                      </RadioGroup>
-                    </fieldset>
-                  </div> */}
-                </form>
-
-                {/* <section aria-labelledby="details-heading" className="mt-12">
-                  <h2 id="details-heading" className="sr-only">
-                    Additional details
-                  </h2>
-
-                  <div className="divide-y divide-gray-200 border-t">
-                    {product.details.map((detail) => (
-                      <Disclosure as="div" key={detail.name}>
-                        {({ open }) => (
-                          <>
-                            <h3>
-                              <DisclosureButton className="group relative flex w-full items-center justify-between py-6 text-left">
-                                <span
-                                  className={classNames(
-                                    open ? "text-indigo-600" : "text-gray-900",
-                                    "text-sm font-medium",
-                                  )}
-                                >
-                                  {detail.name}
-                                </span>
-                                <span className="ml-6 flex items-center">
-                                  {open ? (
-                                    <MinusIcon
-                                      className="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <PlusIcon
-                                      className="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
-                                      aria-hidden="true"
-                                    />
-                                  )}
-                                </span>
-                              </DisclosureButton>
-                            </h3>
-                            <DisclosurePanel
-                              as="div"
-                              className="prose prose-sm pb-6"
-                            >
-                              <ul role="list">
-                                {detail.items.map((item) => (
-                                  <li key={item}>{item}</li>
-                                ))}
-                              </ul>
-                            </DisclosurePanel>
-                          </>
-                        )}
-                      </Disclosure>
-                    ))}
-                  </div>
-                </section> */}
               </div>
             </div>
-            {related.length > 0 ? (
+
+            {related.length > 0 && (
               <section
                 aria-labelledby="related-heading"
                 className="mt-10 border-t border-gray-200 px-4 py-16 sm:px-0"
@@ -352,8 +239,6 @@ export default function AquaDynamicProductComponent() {
                   ))}
                 </div>
               </section>
-            ) : (
-              ""
             )}
           </div>
         </main>
