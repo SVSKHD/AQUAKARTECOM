bcc6ab8fbd7345eed2059f3e635aaef301ec6534
SVSKHD <8svskhd@gmail.com>
2024-08-26 17:38:01 +0530

    card

--- src/components/cards/productCard.js
+++ src/components/cards/productCard.js
@@ -51,13 +51,13 @@ const AquaProductCard = ({ product }) => {
         <span className="isolate inline-flex rounded-md shadow-sm">
           <button
             type="button"
-            className={`relative inline-flex items-center rounded-l-md ${cart ? "bg-green-500" : "bg-white"}  px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:z-10`}
+            className={`relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:z-10`}
             onClick={() => AddAndRemoveCart(product, setAddCart)}
           >
             {cart ? (
               <>
                 <span>Added To Cart</span>
-                <FaCartArrowDown size={25} />
+                <FaCartArrowDown className="text-green-800" size={25} />
               </>
             ) : (
               <>
