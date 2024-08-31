8a0f8543f5d70964017948c38cc5376cde049df0
SVSKHD <8svskhd@gmail.com>
2024-08-31 21:55:32 +0530

    about

--- src/pageComponents/about.js
+++ src/pageComponents/about.js
@@ -67,7 +67,7 @@ const jobOpenings = [
   },
 ];
 
-const AquaAbout() {
+const AquaAbout=()=> {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const seo = {
     title: "Aquakart | About",
