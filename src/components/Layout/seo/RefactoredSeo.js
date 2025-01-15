fe3ceee10802a61eec0d667176054c63020aba74
SVSKHD <8svskhd@gmail.com>
2025-01-15 21:12:42 +0530

    seo

--- src/components/Layout/seo/RefactoredSeo.js
+++ src/components/Layout/seo/RefactoredSeo.js
@@ -27,26 +27,28 @@ const AquaSeoRevamp = ({ path, category, categoryData, subcategory, subcategoryD
         <meta name="keywords" content={keywords} />
         <meta name="description" content={description} />
         <meta name="keyphrases" content={keyphrases} />
-        <meta
-          name="robots"
-          content={`index, ${follow ? "follow" : "nofollow"}`}
-        />
+        <meta name="robots" content={`index, ${follow ? "follow" : "nofollow"}, max-image-preview:large`} />
+
 
         {/* Open Graph (Facebook & LinkedIn) */}
         <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Aquakart" />
         <meta property="og:url" content={url} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={photos} />
+        <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
         {/* Twitter Cards */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content={url} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={photos} />
+<meta name="twitter:site" content="@aquakart8" /> {/* Replace with your handle */}
+<meta name="twitter:creator" content="@aquakart8" />
+<meta name="twitter:url" content={url} />
+<meta name="twitter:title" content={title} />
+<meta name="twitter:description" content={description} />
+<meta name="twitter:image" content={photos} />
 
         {/* Additional Meta Tags */}
         <meta name="author" content="Aquakart" />
