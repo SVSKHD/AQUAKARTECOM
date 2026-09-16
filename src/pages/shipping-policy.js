7e126c02b1a4859cdeb554f878d1126910f78dca
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2026-09-16 20:30:01 +0530

    feat: server render managed SEO for shipping policy

--- src/pages/shipping-policy.js
+++ src/pages/shipping-policy.js
@@ -1,5 +1,10 @@
 import AquaShippingPolicyComponent from "@/pageComponents/shippingpolicy";
+import { createManagedSeoStaticProps } from "@/services/seo";
+
 const AquaShippingPolicy = () => {
   return <AquaShippingPolicyComponent />;
 };
+
+export const getStaticProps = createManagedSeoStaticProps("shipping-policy");
+
 export default AquaShippingPolicy;
